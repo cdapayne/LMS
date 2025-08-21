d626308cfe67edc1aaf65f55a7e676758b989ec8
cdapayne <carlton.payne@hotmail.com>
2025-08-20 21:12:59 -0400

    update charts

--- routes/teacher.js
+++ routes/teacher.js
@@ -20,8 +20,8 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS
+    user: 'noreply@mdts-apps.com',
+    pass: 'c@r,5ysPI@&s'
   }
 });
 const multer = require('multer');
